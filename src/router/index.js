4c724c40f3bd8c935b445ef151e079745561ce9d
Ahmet Faruk Saridanismet <asaridanismet#gmail.com>
2024-08-03 14:32:05 +0300

    Performance Improvemnet

--- src/router/index.js
+++ src/router/index.js
@@ -4,24 +4,24 @@ import { createWebHistory, createRouter } from "vue-router";
 import About from "../pages/About/About.vue"
 import Home from "../pages/Home/Home.vue"
 import Initiatives from "../pages/Initiatives/Initiatives.vue"
-import Impact from "../pages/Impact/Impact.vue"
+// import Impact from "../pages/Impact/Impact.vue"
 import Sponsors from "../pages/Sponsors/Sponsors.vue"
-import Gallery from "../pages/Gallery/Gallery.vue"
+// import Gallery from "../pages/Gallery/Gallery.vue"
 import Technology from "../pages/Technology/Technology.vue"
 import Press from "../pages/Press/Press.vue"
 import Team from "../pages/Team/Team.vue"
-import Play from "../pages/Play/Play.vue"
+// import Play from "../pages/Play/Play.vue"
 import NotFound from "../pages/NotFound/NotFound.vue"
 
 const routes = [
   { path: "/home", redirect:"/"},
   { path: "/", name: "Home", component:Home},
   { path: "/about", name: "About", component:About },
-  { path: "/gallery", name: "Gallery", component:Gallery },
+  { path: "/gallery", name: "Gallery", component:() => import("../pages/Gallery/Gallery.vue") },
   { path: "/contact", name: "Contact" },
-  { path: "/impact", name: "Impact", component:Impact },
+  { path: "/impact", name: "Impact", component:() => import("../pages/Impact/Impact.vue") },
   { path: "/initiatives", name: "Initiatives", component:Initiatives },
-  { path: "/play", name: "Play", component:Play },
+  { path: "/play", name: "Play", component: () => import("../pages/Play/Play.vue") },
   { path: "/press", name: "Press", component:Press },
   { path: "/sponsors", name: "Sponsors", component:Sponsors },
   { path: "/team", name: "Team", component:Team },
