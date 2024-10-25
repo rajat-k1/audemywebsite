758d147be3038fa89538915b1e5c07d8b0e97994
Crystal Yang <106780309+Crustaly@users.noreply.github.com>
2024-10-25 16:04:34 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,4 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/audemywebsite/',  // replace 'your-repo-name' with the name of your GitHub repository
 })
