a4ce3f8c30d4efd8f4b144cef6367fc178f7e123
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-10-29 18:12:15 -0400

    Changed Files

--- src/pages/Impact/CollaboratingSchools/TeachersList/TeachersList.vue
+++ src/pages/Impact/CollaboratingSchools/TeachersList/TeachersList.vue
@@ -1,30 +1,27 @@
 <script setup>
-
 const props = defineProps({
-  smallScreen: Boolean,
-  link: String,
-  name: String
+    smallScreen: Boolean,
+    link: String,
+    name: String,
 });
-
 </script>
 
 <template>
-
-  <div v-if="!smallScreen" class="mx-1 flex flex-row items-center h-auto">
-
-    <div class="">
-
-      <div class="flex flex-col justify-evenly items-center ">
-
-        <!-- TEXT -->
-        <div class="mx-2">
-          <img src="/src/assets/images/impact/starBullet.svg" class="mr-2 inline-block" />
-          <p class="inline text-center text-[16px] text-body-text-color font-poppins tablet:text-[16px] font-[400]" v-html="name"></p>
+    <div v-if="!smallScreen" class="mx-1 flex flex-row items-center h-auto">
+        <div class="">
+            <div class="flex flex-col justify-evenly items-center">
+                <!-- TEXT -->
+                <div class="mx-2">
+                    <img
+                        src="/public/assets/images/impact/starBullet.svg"
+                        class="mr-2 inline-block"
+                    />
+                    <p
+                        class="inline text-center text-[16px] text-body-text-color font-poppins tablet:text-[16px] font-[400]"
+                        v-html="name"
+                    ></p>
+                </div>
+            </div>
         </div>
-
-      </div>
     </div>
-  </div>
-
-
 </template>
