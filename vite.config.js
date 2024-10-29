669e37c5b9e5827f744adce33f110273850ddb71
Crystal Yang <106780309+Crustaly@users.noreply.github.com>
2024-10-29 16:16:46 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/audemywebsite/' : '/', // Replace 'your-repo-name' with your actual repo name
-})
+    plugins: [vue()],
+    base: "/", // Replace 'your-repo-name' with your actual repo name
+});
