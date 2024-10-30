2407a2ab84fc04cb3ae86108eb8a8a061512dc68
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-10-29 21:25:34 -0400

    Updated Routes

--- src/router/index.js
+++ src/router/index.js
@@ -19,65 +19,65 @@ import PolarPairing from "../pages/GameZone/GameZoneList/PolarPairing.vue";
 import SpellingBee from "../pages/GameZone/GameZoneList/SpellingBee.vue";
 
 const routes = [
-  { path: "/home", redirect: "/" },
-  { path: "/", name: "Home", component: Home },
-  { path: "/game-zone", name: "Game-Zone", component: GameZone },
-  { path: "/our-projects", name: "Our-Projects", component: OurProjects },
-  { path: "/impact", name: "Impact", component: Impact },
-  { path: "/about-us", name: "About-Us", component: AboutUs },
-  { path: "/login", name: "LogIn", component: LogIn },
-  { path: "/signup", name: "Sign Up", component: SignUp },
-  { path: "/:pathMatch(.*)*", name: "not-found", component: NotFound },
-  {
-    path: "/game/addition",
-    name: "AdditionGame",
-    component: AdditionGame,
-  },
-  {
-    path: "/game/multiplicationmadness",
-    name: "MultiplicationMadness",
-    component: MultiplicationMadness,
-  },
-  {
-    path: "/game/subtraction",
-    name: "SubtractionGame",
-    component: SubtractionGame,
-  },
-  {
-    path: "/game/vocabVortex",
-    name: "vocabVortex",
-    component: Vocab,
-  },
-  {
-    path: "/game/divisionDuel",
-    name: "DivisionDuel",
-    component: DivisionDuel,
-  },
-  {
-    path: "/game/oddoneout",
-    name: "OddOneOut",
-    component: OddOneOut,
-  },
-  {
-    path: "/game/polarpairing",
-    name: "PolarPairing",
-    component: PolarPairing,
-  },
-  {
-    path: "/game/spelling-bee",
-    name: "SpellingBee",
-    component: SpellingBee,
-  },
-  {
-    path: "/game/spellingbee",
-    name: "SpellingBee",
-    component: SpellingBee,
-  },
+    { path: "/home", redirect: "/" },
+    { path: "/", name: "Home", component: Home },
+    { path: "/game-zone", name: "Game-Zone", component: GameZone },
+    { path: "/our-projects", name: "Our-Projects", component: OurProjects },
+    { path: "/impact", name: "Impact", component: Impact },
+    { path: "/about-us", name: "About-Us", component: AboutUs },
+    { path: "/login", name: "LogIn", component: LogIn },
+    { path: "/signup", name: "Sign Up", component: SignUp },
+    { path: "/:pathMatch(.*)*", name: "not-found", component: NotFound },
+    {
+        path: "/game/addition",
+        name: "AdditionGame",
+        component: AdditionGame,
+    },
+    {
+        path: "/game/multiplicationmadness",
+        name: "MultiplicationMadness",
+        component: MultiplicationMadness,
+    },
+    {
+        path: "/game/subtraction",
+        name: "SubtractionGame",
+        component: SubtractionGame,
+    },
+    {
+        path: "/game/vocabVortex",
+        name: "vocabVortex",
+        component: Vocab,
+    },
+    {
+        path: "/game/divisionDuel",
+        name: "DivisionDuel",
+        component: DivisionDuel,
+    },
+    {
+        path: "/game/oddoneout",
+        name: "OddOneOut",
+        component: OddOneOut,
+    },
+    {
+        path: "/game/polarpairing",
+        name: "PolarPairing",
+        component: PolarPairing,
+    },
+    {
+        path: "/game/spelling-bee",
+        name: "SpellingBee",
+        component: SpellingBee,
+    },
+    {
+        path: "/game/spellingbee",
+        name: "SpellingBee",
+        component: SpellingBee,
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    // history: createWebHistory(),
+    routes,
 });
 
 export default router;
