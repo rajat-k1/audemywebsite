1548b17c3c4aec586666ad1a328e7f90c6c3be11
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-11-25 12:43:32 -0500

    Updated Hero

--- src/pages/OurProjects/Hero/Hero.vue
+++ src/pages/OurProjects/Hero/Hero.vue
@@ -3,7 +3,9 @@
 <template>
     <div class="flex justify-center my-40">
         <div class="max-w-[85%] w-full overflow-hidden">
-            <div class="w-full h-full flex justify-start">
+            <div
+                class="w-full h-full flex justify-start mobile:flex-col mobile:items-center"
+            >
                 <div
                     class="flex flex-col justify-around items-center max-w-[510px] h-full gap-8 pb-5"
                 >
@@ -13,13 +15,15 @@
                         Discover how we make education accessible
                     </h2>
                     <h4
-                        class="text-left text-body-text-color font-poppins text-[20px] tablet:text-[16px] font-[400]"
+                        class="text-left text-body-text-color font-poppins text-[20px] tablet:text-[16px] mobile:text-center font-[400]"
                     >
                         Our innovative technologies and educational initiatives
                         provide fun learning for blind and visually impaired
                         individuals.
                     </h4>
-                    <div class="flex justify-start w-full">
+                    <div
+                        class="flex justify-start w-full mobile:justify-center"
+                    >
                         <a
                             href="#div_events"
                             class="font-poppins font-semibold px-9 py-4 border-[1.5px] border-[#0C0D0D] rounded-[8px] bg-primary-color hover:bg-[#0C587D] duration-300 text-base text-center text-[#fff] shadow-[3px_4px_0px_#0C0D0D]"
@@ -28,12 +32,12 @@
                         </a>
                     </div>
                 </div>
+                <img
+                    src="/assets/images/our-projects/header/banner.svg"
+                    alt=""
+                    class="absolute -top-3 right-0 -z-10 w-[50rem] h-auto mobile:relative mobile:top-0 mobile:right-auto mobile:w-full mobile:h-auto"
+                />
             </div>
-            <img
-                src="/assets/images/our-projects/header/banner.svg"
-                alt=""
-                class="absolute -top-3 right-0 -z-10 w-[50rem] h-auto"
-            />
         </div>
     </div>
 </template>
