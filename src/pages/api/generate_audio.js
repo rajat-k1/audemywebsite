69da42251bd6318ad5ffe52abfa1bf3981d340d4
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-12-30 15:41:38 -0500

    Testing

--- src/pages/api/generate_audio.js
+++ src/pages/api/generate_audio.js
@@ -16,33 +16,33 @@ export default async function handler(req, res) {
         return res.status(200).json({ message: "Welcome to the TTS API" });
     }
     if (req.method === "POST") {
-        print("POST request received");
+        console.log("POST request received");
         const { text } = req.body;
 
-        if (!text) {
-            return res.status(400).json({ error: "Text is required" });
-        }
+        // if (!text) {
+        //     return res.status(400).json({ error: "Text is required" });
+        // }
 
-        try {
-            const request = {
-                input: { text },
-                voice: {
-                    languageCode: "en-US",
-                    ssmlGender: "NEUTRAL",
-                    name: "en-US-Standard-H",
-                },
-                audioConfig: { audioEncoding: "MP3" },
-            };
+        // try {
+        //     const request = {
+        //         input: { text },
+        //         voice: {
+        //             languageCode: "en-US",
+        //             ssmlGender: "NEUTRAL",
+        //             name: "en-US-Standard-H",
+        //         },
+        //         audioConfig: { audioEncoding: "MP3" },
+        //     };
 
-            const [response] = await client.synthesizeSpeech(request);
+        //     const [response] = await client.synthesizeSpeech(request);
 
-            res.setHeader("Content-Type", "audio/mpeg");
-            res.status(200).send(response.audioContent);
-        } catch (error) {
-            console.error("Error generating TTS:", error);
-            res.status(500).json({ error: "Failed to generate TTS" });
-        }
-    } else {
-        res.status(405).json({ error: "Method Not Allowed" });
+        //     res.setHeader("Content-Type", "audio/mpeg");
+        //     res.status(200).send(response.audioContent);
+        // } catch (error) {
+        //     console.error("Error generating TTS:", error);
+        //     res.status(500).json({ error: "Failed to generate TTS" });
+        // }
+        // } else {
+        //     res.status(405).json({ error: "Method Not Allowed" });
     }
 }
