6b59da6fdf1a64cf0060c273680b115f339c7494
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-12-30 16:23:53 -0500

    Changed Code

--- src/pages/api/generate_audio.js
+++ src/pages/api/generate_audio.js
@@ -3,13 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 // Ensure that the environment variable is set
-// if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
-//     console.error(
-//         "GOOGLE_APPLICATION_CREDENTIALS environment variable is not set."
-//     );
-//     process.exit(1);
-// }
-// const client = new TextToSpeechClient();
+if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+    console.error(
+        "GOOGLE_APPLICATION_CREDENTIALS environment variable is not set."
+    );
+    process.exit(1);
+}
+const client = new TextToSpeechClient();
 
 export default async function handler(req, res) {
     if (req.method === "GET") {
@@ -19,30 +19,30 @@ export default async function handler(req, res) {
         console.log("POST request received");
         const { text } = req.body;
 
-        // if (!text) {
-        //     return res.status(400).json({ error: "Text is required" });
-        // }
+        if (!text) {
+            return res.status(400).json({ error: "Text is required" });
+        }
 
-        // try {
-        //     const request = {
-        //         input: { text },
-        //         voice: {
-        //             languageCode: "en-US",
-        //             ssmlGender: "NEUTRAL",
-        //             name: "en-US-Standard-H",
-        //         },
-        //         audioConfig: { audioEncoding: "MP3" },
-        //     };
+        try {
+            const request = {
+                input: { text },
+                voice: {
+                    languageCode: "en-US",
+                    ssmlGender: "NEUTRAL",
+                    name: "en-US-Standard-H",
+                },
+                audioConfig: { audioEncoding: "MP3" },
+            };
 
-        //     const [response] = await client.synthesizeSpeech(request);
+            const [response] = await client.synthesizeSpeech(request);
 
-        //     res.setHeader("Content-Type", "audio/mpeg");
-        //     res.status(200).send(response.audioContent);
-        // } catch (error) {
-        //     console.error("Error generating TTS:", error);
-        //     res.status(500).json({ error: "Failed to generate TTS" });
-        // }
-        // } else {
-        //     res.status(405).json({ error: "Method Not Allowed" });
+            res.setHeader("Content-Type", "audio/mpeg");
+            res.status(200).send(response.audioContent);
+        } catch (error) {
+            console.error("Error generating TTS:", error);
+            res.status(500).json({ error: "Failed to generate TTS" });
+        }
+    } else {
+        res.status(405).json({ error: "Method Not Allowed" });
     }
 }
