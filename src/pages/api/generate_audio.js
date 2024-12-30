a8b01c5f740bd06c30db4702e94f062d57b1896a
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-12-30 15:04:27 -0500

    Updated Code

--- src/pages/api/generate_audio.js
+++ src/pages/api/generate_audio.js
@@ -6,6 +6,9 @@ dotenv.config();
 const client = new TextToSpeechClient();
 
 export default async function handler(req, res) {
+    if (req.method === "GET") {
+        return res.status(200).json({ message: "Welcome to the TTS API" });
+    }
     if (req.method === "POST") {
         const { text } = req.body;
 
