017c38c0640e939bebabb265106af579f2bad42d
Joe <V.josephraj26@gmail.com>
2025-01-27 16:32:54 -0500

    Improved GameList in GameProgress Tab

--- src/pages/GameZone/GameProgress/GameProgress.vue
+++ src/pages/GameZone/GameProgress/GameProgress.vue
@@ -6,82 +6,42 @@
         </div>
 
         <!-- Language lab Journey -->
-        <div class="flex flex-col gap-8 border-2 items-center border-black rounded-xl w-3/4 py-10">
+        <div class="flex flex-col border-2 items-center border-black rounded-xl w-3/4 py-10">
             <!-- Title -->
             <div class="text-[25px]">
                 My Language Lab Journey
-            </div>
-
-            <div>
-                <div>{{ languageGames }}</div>
             </div>
             
             <!-- Games -->
             <div class="flex flex-row items-center w-full">
                 <!-- Arrow Swipe Left -->
                 <div class="flex justify-center w-1/6">
-                    <button class="text-4xl h-full">
-                        <img src="/assets/gameImages/buttons/arrow-slide-back.svg" class="h-12 hover:bg-gray-300"/>
+                    <button>
+                        <img src="/assets/gameImages/buttons/arrow-slide-back.svg" class="h-14"/>
                     </button>
                 </div>
                 <!-- Horizontal Game List -->
-                <div class="flex overflow-auto gap-16 w-4/6 p-2">
-                    
-                    <div>
-                        <img src="/assets/myProgress/stage2.svg" class="object-fill"/>
-                        <div>GameName</div>
-                        <div>Level</div>
-                    </div>
-                    <div>
-                        <img src="/assets/myProgress/stage1.svg" class="object-fill"/>
-                        <div>GameName</div>
-                        <div>Level</div>
-                    </div>
-                    <div>
-                        <img src="/assets/myProgress/stage2.svg" class="object-fill"/>
-                        <div>GameName</div>
-                        <div>Level</div>
-                    </div>
-                    <div>
-                        <img src="/assets/myProgress/stage3.svg" class="object-fill"/>
-                        <div>GameName</div>
-                        <div>Level</div>
-                    </div>
-                    <div>
-                        <img src="/assets/myProgress/stage1.svg" class="object-fill"/>
-                        <div>GameName</div>
-                        <div>Level</div>
-                    </div>
-                    <div>
-                        <img src="/assets/myProgress/stage3.svg" class="object-fill"/>
-                        <div>GameName</div>
-                        <div>Level</div>
-                    </div>
-                    <div>
-                        <img src="/assets/myProgress/stage2.svg" class="object-fill"/>
-                        <div>GameName</div>
-                        <div>Level</div>
-                    </div>
-                    <div>
-                        <img src="/assets/myProgress/stage1.svg" class="object-fill"/>
-                        <div>GameName</div>
-                        <div>Level</div>
+                <div class="flex flex-row overflow-auto gap-16 w-4/6 p-2">
+                    <div v-for="game in languageGames" class="basis-1/4 flex-none">
+                        <div><img src="/assets/myProgress/stage1.svg" class="object-fill"/></div>
+                        <div class="flex justify-center pt-2">{{ game.title }}</div>
+                        <div class="flex justify-center text-gray-500">Level</div>
                     </div>
                 </div>
                 <!-- Arrow Swipe Right -->
                 <div class="flex justify-center w-1/6">
                     <button>
-                        <img src="/assets/gameImages/buttons/arrow-slide-forward.svg" class="h-12 hover:bg-gray-300"/>
+                        <img src="/assets/gameImages/buttons/arrow-slide-forward.svg" class="h-14"/>
                     </button>
                 </div>
             </div>
 
             <!-- Progress Status -->
-            <div class="flex flex-row gap-12 px-20 items-center w-full">
+            <div class="flex flex-row gap-12 px-20 pt-10 items-center w-full">
                 <!-- Percentage -->
                 <div class="w-1/4">
                     <div>25% achieved </div>
-                    <div>5 more levels to unlock</div>
+                    <div class="text-gray-500 text-xs">5 more levels to unlock</div>
                 </div>
                 <!-- Progress Bar -->
                 <div class="w-3/4 bg-[#a999dd] rounded-xl h-3">
@@ -98,8 +58,27 @@
             </div>
             
             <!-- Games -->
-            <div>
-                <img src="/public/assets/gameImages/buttons/gameButtons/animalAddition.svg" />
+            <div class="flex flex-row items-center w-full">
+                <!-- Arrow Swipe Left -->
+                <div class="flex justify-center w-1/6">
+                    <button>
+                        <img src="/assets/gameImages/buttons/arrow-slide-back.svg" class="h-14"/>
+                    </button>
+                </div>
+                <!-- Horizontal Game List -->
+                <div class="flex flex-row overflow-auto gap-16 w-4/6 p-2">
+                    <div v-for="game in mathGames" class="basis-1/4 flex-none">
+                        <div><img src="/assets/myProgress/stage2.svg" class="object-fill"/></div>
+                        <div class="flex justify-center pt-2">{{ game.title }}</div>
+                        <div class="flex justify-center text-gray-500">Level</div>
+                    </div>
+                </div>
+                <!-- Arrow Swipe Right -->
+                <div class="flex justify-center w-1/6">
+                    <button>
+                        <img src="/assets/gameImages/buttons/arrow-slide-forward.svg" class="h-14"/>
+                    </button>
+                </div>
             </div>
 
             <!-- Progress Status -->
@@ -107,7 +86,7 @@
                 <!-- Percentage -->
                 <div class="w-1/4">
                     <div>55% achieved </div>
-                    <div>5 more levels to unlock</div>
+                    <div class="text-gray-500 text-xs">5 more levels to unlock</div>
                 </div>
                 <!-- Progress Bar -->
                 <div class="w-3/4 bg-[#79e2e2] rounded-xl h-3">
@@ -122,17 +101,12 @@
 import { ref } from 'vue';
 import { getLanguageGames, getMathGames } from "../GameDB.js";
 
-let languageGames = ref();
-languageGames = getLanguageGames();
-
-for (let i = 0; i < languageGames.length; i++) {
-    console.log(languageGames[i].title);
-}
-
 export default {
   setup() {
     const userName = ref("[Name]");
-    return { userName };
+    let languageGames = ref(getLanguageGames());
+    let mathGames = ref(getMathGames());
+    return { userName, languageGames, mathGames };
   }
 };
 </script>
