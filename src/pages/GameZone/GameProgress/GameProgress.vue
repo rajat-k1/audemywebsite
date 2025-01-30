d8a114d0dcbb4d14e493d4f3f26c36238449775c
Joe <V.josephraj26@gmail.com>
2025-01-30 14:08:53 -0500

    Redesigned MyProgress tab to display a "Work in Progress" message with a new layout.

--- src/pages/GameZone/GameProgress/GameProgress.vue
+++ src/pages/GameZone/GameProgress/GameProgress.vue
@@ -1,108 +1,15 @@
 <template>
-    <div class="flex flex-col gap-8 items-center py-4">
-        <!-- User Greeting -->
-        <div class="text-[30px]">
-            {{ userName }}, unlock new levels with Carl!
+    <div class="flex flex-col text-[30px] text-center border-2 border-black rounded-xl p-12">
+        <div class="flex justify-center"><img src="/public/assets/images/impact/globe 1.svg" alt="Under Construction" /></div>
+        <div class="font-bold">
+            W<span class="text-[24px]">ORK</span> 
+            I<span class="text-[24px]">N</span> 
+            P<span class="text-[24px]">ROGRESS</span>
         </div>
-
-        <!-- Language lab Journey -->
-        <div class="flex flex-col border-2 items-center border-black rounded-xl w-3/4 py-10">
-            <!-- Title -->
-            <div class="text-[25px]">
-                My Language Lab Journey
-            </div>
-            
-            <!-- Games -->
-            <div class="flex flex-row items-center w-full">
-                <!-- Arrow Swipe Left -->
-                <div class="flex justify-center w-1/6">
-                    <button @click="langScrollLeft" class=" py-32  px-4">
-                        <img src="/assets/gameImages/buttons/arrow-slide-back.svg" class="h-14"/>
-                    </button>
-                </div>
-                <!-- Horizontal Game List -->
-                <div class="flex flex-row overflow-auto gap-16 w-4/6 p-2 lang_gameList">
-                    <div v-for="game in languageGames" class="basis-1/4 flex-none">
-                        <div><img src="/assets/myProgress/stage1.svg" class="object-fill"/></div>
-                        <div class="flex justify-center pt-2">{{ game.title }}</div>
-                        <div class="flex justify-center text-gray-500">Level</div>
-                    </div>
-                </div>
-                <!-- Arrow Swipe Right -->
-                <div class="flex justify-center w-1/6">
-                    <button @click="langScrollRight" class="py-32  px-4">
-                        <img src="/assets/gameImages/buttons/arrow-slide-forward.svg" class="h-14"/>
-                    </button>
-                </div>
-            </div>
-        </div>
-
-        <!-- Math Magic Journey -->
-        <div class="flex flex-col gap-8 border-2 items-center border-black rounded-xl w-3/4 py-10">
-            <!-- Title -->
-            <div class="text-[25px]">
-                My Math Magic Journey
-            </div>
-            
-            <!-- Games -->
-            <div class="flex flex-row items-center w-full">
-                <!-- Arrow Swipe Left -->
-                <div class="flex justify-center w-1/6">
-                    <button @click="mathScrollLeft" class="py-32  px-4">
-                        <img src="/assets/gameImages/buttons/arrow-slide-back.svg" class="h-14"/>
-                    </button>
-                </div>
-                <!-- Horizontal Game List -->
-                <div class="flex flex-row overflow-auto gap-16 w-4/6 p-2 math_gameList">
-                    <div v-for="game in mathGames" class="basis-1/4 flex-none">
-                        <div><img src="/assets/myProgress/stage2.svg" class="object-fill"/></div>
-                        <div class="flex justify-center pt-2">{{ game.title }}</div>
-                        <div class="flex justify-center text-gray-500">Level</div>
-                    </div>
-                </div>
-                <!-- Arrow Swipe Right -->
-                <div class="flex justify-center w-1/6">
-                    <button @click="mathScrollRight" class="py-32  px-4">
-                        <img src="/assets/gameImages/buttons/arrow-slide-forward.svg" class="h-14"/>
-                    </button>
-                </div>
-            </div>
-        </div>
+        <div class="text-[20px] text-gray-500">Coming Soon!</div>
     </div>
 </template>
 
 <script>
-import { ref } from 'vue';
-import { getLanguageGames, getMathGames } from "../GameDB.js";
 
-export default {
-  setup() {
-    const userName = ref("[Name]");
-    let languageGames = ref(getLanguageGames());
-    let mathGames = ref(getMathGames());
-    return { userName, languageGames, mathGames };
-  },
-  methods: {
-    langScrollRight() {
-      let content = document.querySelector(".lang_gameList");
-      console.log(content);
-      content.scrollLeft += 100;
-    },
-    langScrollLeft() {
-      let content = document.querySelector(".lang_gameList");
-      console.log(content);
-      content.scrollLeft -= 100;
-    },
-    mathScrollRight() {
-      let content = document.querySelector(".math_gameList");
-      console.log(content);
-      content.scrollLeft += 100;
-    },
-    mathScrollLeft() {
-      let content = document.querySelector(".math_gameList");
-      console.log(content);
-      content.scrollLeft -= 100;
-    },
-  },
-};
 </script>
