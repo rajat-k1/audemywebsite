cf73773925308326dc65c3f634513e7b7db82627
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2025-03-04 12:36:50 -0500

    Updated Get USer

--- src/pages/api/db/get_user.js
+++ src/pages/api/db/get_user.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
         });
 
         if (!user) {
-            return res.status(404).json({ error: "User not found" });
+            return res.status(200).json({ error: "User not found" });
         }
 
         return res.status(200).json(user);
