35e3c27df002d3fe5e02244103c2b0209e4eb7cb
Crystal Yang <106780309+Crustaly@users.noreply.github.com>
2025-03-12 10:33:46 -0500

    Changed the order of games, and changed a few colors

--- src/pages/GameZone/GameDB.js
+++ src/pages/GameZone/GameDB.js
@@ -2,42 +2,34 @@ const basedPath = "/assets/gameImages/buttons/gameButtons/";
 export const getLanguageGames = () => {
     return [
         {
-            icon: basedPath + "spellingBee.svg",
-            title: "Spelling Bee",
-            //url: "/game/spelling-bee",
-            url: "/game/spellingbee",
+            icon: basedPath + "definition.svg",
+            title: "Definition Detective",
+            url: "/game/definitionDetective",
             textColor: "#323232",
-            bgColor: "#F7DA21",
+            bgColor: "#06d4f9",
             bgDecoration: true,
-            bgImage: "/assets/gameImages/bg-spelling.svg",
-            description: "Buzz your way to spelling mastery!",
+            bgImage: "/assets/gameImages/bg-definition.svg",
+            description: "Find the correct word based on the definition.",
         },
         {
-            icon: basedPath + "strawberry.svg",
-            title: "Polar Pairing",
-            url: "/game/polarpairing",
+            icon: basedPath + "pos.svg",
+            title: "Part of Speech",
+            url: "/game/partofspeech",
             textColor: "#323232",
-            bgColor: "#FF9A22",
-            description: "Test your vocabulary with polar opposites.",
+            bgColor: "#00d353",
+            description: "Noun, verb, or adjective?",
         },
         {
-            icon: basedPath + "oddOneOut.svg",
-            title: "Odd One Out",
-            url: "/game/oddoneout",
+            icon: basedPath + "color-game.svg",
+            title: "Color Game",
+            url: "/game/colorgame",
             textColor: "#323232",
-            bgColor: "#FF82E0",
-            description: "Can you find what doesn’t belong?",
+            bgColor: "#cf88ff",
+            description: "Name the color of a given object!",
         },
-        {
-            icon: basedPath + "book.svg",
-            title: "Vocabulary Vortex",
-            url: "/game/vocabVortex",
-            textColor: "#323232",
-            bgColor: "#F56A6C",
-            bgDecoration: true,
-            bgImage: "/assets/gameImages/bg-vocab.svg",
-            description: "Dive into the vocabulary vortex and match the word to its definition.",
-        },
+       
+       
+       
         {
             icon: basedPath + "book.svg",
             title: "Syllable Sorting",
@@ -49,30 +41,43 @@ export const getLanguageGames = () => {
             description: "Can you count the syllables in a given word?",
         },
         {
-            icon: basedPath + "color-game.svg",
-            title: "Color Game",
-            url: "/game/colorgame",
+            icon: basedPath + "book.svg",
+            title: "Vocabulary Vortex",
+            url: "/game/vocabVortex",
             textColor: "#323232",
-            bgColor: "#0EA1EA",
-            description: "Name the color of a given object!",
-        },
-        {
-            icon: basedPath + "pos.svg",
-            title: "Part of Speech",
-            url: "/game/partofspeech",
-            textColor: "#323232",
-            bgColor: "#00C275",
-            description: "Noun, verb, or adjective?",
-        },
-        {
-            icon: basedPath + "definition.svg",
-            title: "Definition Detective",
-            url: "/game/definitionDetective",
-            textColor: "#323232",
-            bgColor: "#28B8CA",
+            bgColor: "#ff7373",
             bgDecoration: true,
-            bgImage: "/assets/gameImages/bg-definition.svg",
-            description: "Find the correct word based on the definition.",
+            bgImage: "/assets/gameImages/bg-vocab.svg",
+            description: "Match the word to its definition.",
+        },
+        {
+            icon: basedPath + "strawberry.svg",
+            title: "Polar Pairing",
+            url: "/game/polarpairing",
+            textColor: "#323232",
+            bgColor: "#ff834d",
+            description: "Test your vocabulary with polar opposites.",
+        },
+        
+        {
+            icon: basedPath + "oddOneOut.svg",
+            title: "Odd One Out",
+            url: "/game/oddoneout",
+            textColor: "#323232",
+            bgColor: "#FF82E0",
+            description: "Can you find what doesn’t belong?",
+        },
+      
+        {
+            icon: basedPath + "spellingBee.svg",
+            title: "Spelling Bee",
+            //url: "/game/spelling-bee",
+            url: "/game/spellingbee",
+            textColor: "#323232",
+            bgColor: "#F7DA21",
+            bgDecoration: true,
+            bgImage: "/assets/gameImages/bg-spelling.svg",
+            description: "Buzz your way to spelling mastery!",
         },
     ];
 };
@@ -80,24 +85,23 @@ export const getLanguageGames = () => {
 export const getMathGames = () => {
     return [
         {
-            icon: basedPath + "subtraction.svg",
-            title: "Subtraction Safari",
-            url: "/game/subtraction",
+            icon: basedPath + "ff.svg",
+            title: "Fruit Frenzy",
+            url: "/game/fruitfrenzy",
             textColor: "#323232",
-            bgColor: "#28B8CA",
-            bgDecoration: true,
-            bgImage: "/assets/gameImages/bg-sub.svg",
-            description: "Subtract animals in safari scenarios!",
-        },
+            bgColor: "#ff7d7d   ",
+            description: "Practice addition by counting the fruits!",
+        },  
         {
-            icon: basedPath + "div.svg",
-            title: "Division Duel",
-            url: "game/DivisionDuel",
+            icon: basedPath + "ss.svg",
+            title: "Shape Shark",
+            url: "/game/shapeshark",
             textColor: "#323232",
-            bgColor: "#00C275",
-            description: "Divide the given numers!",
+            bgColor: "#0EA1EA",
+            description: "Identify the shape based on its sides.",
         },
-        {
+       
+         {
             icon: basedPath + "addition.svg",
             title: "Animal Addition",
             url: "/game/addition",
@@ -105,6 +109,19 @@ export const getMathGames = () => {
             bgColor: "#FF82E0",
             description: "Add up the animals!",
         },
+        {
+            icon: basedPath + "subtraction.svg",
+            title: "Subtraction Safari",
+            url: "/game/subtraction",
+            textColor: "#323232",
+            bgColor: "#4ad9de",
+            bgDecoration: true,
+            bgImage: "/assets/gameImages/bg-sub.svg",
+            description: "Subtract animals in safari scenarios!",
+        },
+       
+       
+       
         {
             icon: basedPath + "mul.svg",
             title: "Multiplication Madness",
@@ -116,6 +133,14 @@ export const getMathGames = () => {
             description: "Can you multiply numbers?",
         },
         {
+            icon: basedPath + "div.svg",
+            title: "Division Duel",
+            url: "game/DivisionDuel",
+            textColor: "#323232",
+            bgColor: "#00C275",
+            description: "Divide the given numers!",
+        },
+        {
             icon: basedPath + "mm.svg",
             title: "Monkey Madness",
             url: "/game/monkeymadness",
@@ -125,22 +150,8 @@ export const getMathGames = () => {
             bgImage: "/assets/gameImages/bg-mm.svg",
             description: "Help the monkeys split their bananas!",
         },
-        {
-            icon: basedPath + "ss.svg",
-            title: "Shape Shark",
-            url: "/game/shapeshark",
-            textColor: "#323232",
-            bgColor: "#0EA1EA",
-            description: "Identify the shape based on its sides.",
-        },
-        {
-            icon: basedPath + "ff.svg",
-            title: "Fruit Frenzy",
-            url: "/game/fruitfrenzy",
-            textColor: "#323232",
-            bgColor: "#FF9A22",
-            description: "How many total fruits are there?",
-        },  
+      
+        
         {
             icon: basedPath + "cc.svg",
             title: "Car Counting",
