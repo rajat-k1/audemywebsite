b25fb9bf5c94893ea69c2efdc967163c5e13aded
Joe <V.josephraj26@gmail.com>
2025-04-14 11:11:30 -0400

    enhancement: login/logout logic in GamePagesHeader.vue and minor style changes

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -78,11 +78,11 @@
                     to="/login"
                     class="flex justify-center items-center login-button text-white font-bold py-3 px-6 rounded-lg border-[1.5px] shadow-[3px_4px_0px_#0C0D0D] border-black bg-[#087BB4] hover:bg-[#0C587D]"
                     >
+                    Log in &nbsp;
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M5 12h14"></path>
                         <path d="m12 5 7 7-7 7"></path>
                     </svg>
-                    &nbsp;Log in
                 </router-link>
             </div>
         </nav>
--- src/pages/Header/GamePagesHeader.vue
+++ src/pages/Header/GamePagesHeader.vue
@@ -1,6 +1,6 @@
 <!-- GamePagesHeader.vue -->
 <template>
-    <header class="header">
+    <header class="header border">
       <!-- Left side - Menu and Logo -->
       <div class="header-left">
         <router-link to="/" class="logo-link">
@@ -14,43 +14,70 @@
   
       <!-- Right side - Navigation and Login -->
       <div class="header-right">
-        <nav class="navigation">
+        
+        <div v-if="userSession">
+            <button
+              class="flex justify-center items-center bg-[#FE892A] text-black font-bold py-3 px-6 rounded-lg border-[1.5px] shadow-[3px_4px_0px_#0C0D0D] border-black hover:bg-[#D6711F]"
+              @click="logout"
+            >Log out&nbsp;
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M5 12h14"></path>
+                <path d="m12 5 7 7-7 7"></path>
+              </svg>
+            </button>
+        </div>
+        <div v-else>
           <router-link
-            v-for="item in navigationItems"
-            :key="item.path"
-            :to="item.path"
-            class="nav-link"
+            to="/login"
+            class="flex justify-center items-center login-button text-white font-bold py-3 px-6 rounded-lg border-[1.5px] shadow-[3px_4px_0px_#0C0D0D] border-black bg-[#087BB4] hover:bg-[#0C587D]"
           >
-            {{ item.name }}
+            Log in
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="m12 5 7 7-7 7"></path>
+            </svg>
           </router-link>
-        </nav>
-        <!--router-link
-          to="/login"
-          class="login-button text-white font-bold py-2 px-4 rounded-lg shadow-md hover:bg-[#0d5f8b]"
-        >
-          Login
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M5 12h14"></path>
-            <path d="m12 5 7 7-7 7"></path>
-          </svg>
-        </router-link-->
+        </div>
       </div>
     </header>
+    <!-- Alert -->
+    <div v-if="showAlert" class="flex justify-between w-1/4 bg-green-100 border border-green-400 text-green-700 px-4 py-3 z-50 rounded fixed bottom-5 right-5 transition-opacity duration-300 ease-in-out transform" role="alert">
+        <span class="block sm:inline"><strong class="font-bold">Log out</strong> successful!</span>
+        <button @click="closeAlert">   
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+        </button>
+    </div>
+
   </template>
   
-  <script>
-  export default {
-    name: 'GamePagesHeader',
-    data() {
-      return {
-        navigationItems: [
-          //{ name: 'Home', path: '/' },
-          //{ name: 'About us', path: '/about-us' },
-          { name: 'Game Zone', path: '/game-zone' }
-        ]
-      }
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import Cookies from "js-cookie";
+
+  
+  const userSession = ref(null);
+  const showAlert = ref(false);
+
+  const logout = () => {
+    Cookies.remove("audemyUserSession");
+    userSession.value = null;
+    showAlert.value = true;
+    setTimeout(() => {
+        showAlert.value = false;
+    }, 3000);
+  };
+
+  onMounted(() => {
+    const session = Cookies.get("audemyUserSession");
+    if (session) {
+        const parsed = JSON.parse(session);
+        console.log("Parsed session:", parsed);
+        userSession.value = parsed;
     }
-  }
+    
+});
   </script>
   
   <style scoped>
